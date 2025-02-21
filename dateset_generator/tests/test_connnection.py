3e7899e3418e2c7aa93198bbb08eb1bec2d396f5
brhenri-mr <superbr55@gmail.com>
2025-02-20 23:21:02 -0300

    comm027

--- dateset_generator/tests/test_connnection.py
+++ dateset_generator/tests/test_connnection.py
@@ -1,11 +1,12 @@
 
-
+# Eles todos tem a mesma taxa de erro de 0,8 % muito estranho --> Era a espessura da chapa eu considerei como sendo 6.3 nas contas a mão
+# e no programa está como 6.35 e da examente essa difrerença em porcentagem
 def test_blockShear(conexao):
     '''
     Função para test do blockshear na conexão
     '''
     tol = 10E-3
-    assert conexao.blockShear().magnitude - 307.0667 < 10E-3, f'Valor de BlockShear está errado {conexao.blockShear().magnitude}'
+    assert abs(conexao.blockShear().magnitude - 309.50) < 10E-3, f'Valor de BlockShear está errado {conexao.blockShear().magnitude}'
 
     
 
@@ -13,7 +14,8 @@ def test_plateShear(conexao):
     '''
     Função para test do plateShear
     '''
-    assert conexao.plateShear().magnitude == 205.8, f'Valor de plateShear está errado {conexao.plateShear().magnitude}'
+    tol = 10E-3
+    assert abs(conexao.plateShear().magnitude - 207.433)<tol, f'Valor de plateShear está errado {conexao.plateShear().magnitude}'
     
 
 """
@@ -30,7 +32,7 @@ def test_plateCrush(conexao):
     Função para test do plateCrush
     '''
     tol = 10E-3
-    assert abs(conexao.plateCrush()[0].magnitude - 170.52) < tol, f'Valor de plateCrush está errado {conexao.plateCrush()[0].magnitude}'
+    assert abs(conexao.plateCrush()[0].magnitude - 171.87) < tol, f'Valor de plateCrush está errado {conexao.plateCrush()[0].magnitude}'
 
 
 def test_boltShear(conexao):
